772e3e39bce3e6f73bc7259ab1ed1f1dd24e143c
Ilia <isinetov@mail.ru>
2024-04-22 19:59:36 +0400

    Задание 5.1.6. Модифицируйте метод из скринкаста:  после ввода массива с клавиатуры необходимо отсортировать массив и вывести его на экран.

---
+++ Task 5_1_6/Task 5_1_6.cs
@@ -0,0 +1,34 @@
+﻿class Task_5_1_6 
+{
+    static int[] GetArrayFromConsole()
+    {
+        var result = new int[5];
+        int current = 0;
+        for (int i = 0; i < result.Length; i++)
+        {
+            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+            result[i] = int.Parse(Console.ReadLine());
+        }
+        for (int j = 0; j < result.Length; j++) 
+        {
+            for (int k = j; k < result.Length; k++) 
+            {
+                if (result[j] > result[k]) 
+                {
+                    current = result[j];
+                    result[j] = result[k];
+                    result[k] = current;
+                }
+            }
+        }
+        return result;
+    }
+    static void Main() 
+    {
+        var arr = GetArrayFromConsole();
+        foreach (var item in arr) 
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
