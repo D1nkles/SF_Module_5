07d30cc43f6e8003ac32ebb80b71133dfb4ff07b
Ilia <isinetov@mail.ru>
2024-04-22 19:28:23 +0400

    Задание 5.1.5. Напишите программу, которая в цикле вызывает метод ShowColor(), записывает его значение в массив из трех цветов favcolors, а потом отображает значения этого массива.

---
+++ Task 5_1_5/Task 5_1_5.cs
@@ -0,0 +1,23 @@
+﻿class Task_5_1_5 
+{
+    static string ShowColor() 
+    {
+        string color;
+        Console.Write("Введите ваш любимый цвет: ");
+        color = Console.ReadLine();
+        return color;
+    }
+    static void Main(string[] args) 
+    {
+        var favcolors = new string[3];
+        for (int i = 0; i < favcolors.Length; i++) 
+        {
+            favcolors[i] = ShowColor();
+        }
+        Console.WriteLine("====================================================");
+        foreach (var color in favcolors) 
+        {
+            Console.WriteLine(color);
+        }
+    }
+}
