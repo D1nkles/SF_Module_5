d168ff684fd2d3ae9f9d3a599ee03e7adf01c757
Ilia <isinetov@mail.ru>
2024-04-22 18:40:02 +0400

    Добавьте файлы проекта.

---
+++ Task_1/Task_1.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Net.Http.Headers;
+
+class Task_1 
+{
+    static void Main(string[] args) 
+    {
+        (string Name, string[] Dishes)User;
+
+        Console.Write("Введите имя пользователя: ");
+        User.Name = Console.ReadLine();
+
+        User.Dishes = new string[5];
+        Console.WriteLine("Введите пять ваших любимых блюд...");
+        for (int i = 0; i < User.Dishes.Length; i++) 
+        {
+            Console.Write($"Любимое блюдо №{i + 1}: ");
+            User.Dishes[i] = Console.ReadLine();
+        }
+    }
+}
