c493aa322f9b7bf8238b6184ad50b8a93f9a00c4
Ilia <isinetov@mail.ru>
2024-04-23 21:29:32 +0400

    Задание 5.5.8. Необходимо написать рекурсивный метод, который возводит введенное число N типа int в указанную степень pow типа byte.

--- SF_Module_5/Program.cs
+++ SF_Module_5/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿static int Factorial(int x)
+{
+    if (x == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return x * Factorial(x - 1);
+    }
+}
+int y = Factorial(20);
+Console.WriteLine(y);
---
+++ Task 5_5_8/Task 5_5_8.cs
@@ -0,0 +1,28 @@
+﻿class Task_5_5_8
+{
+    static int PowerUp(int N, byte pow)
+{
+        if (pow == 0) 
+        {
+            return 1;
+        }
+        else 
+        {
+            if (pow == 1) 
+            {
+                return N;
+            }
+            else 
+            {
+                return N * PowerUp(N, --pow);
+            }
+        }
+}
+
+
+    
+    static void Main(string[] args) 
+    {
+        int a = PowerUp(2, 4);
+    }
+}
